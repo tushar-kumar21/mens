77754e89ae6c34972239a146a6b6b2ca865b0776
tushar-kumar <tusharcr4@gmail.com>
2023-07-23 10:34:49 +0530

    sdsd

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("./db/conn")
+const cors = require("cors")
+const router = require("./routers/men")
+
+const app = express()
+const port = process.env.PORT || 3000;
+app.use(
+    cors({
+        origin:[process.env.FRONTEND_URI],
+        method:["GET","POST","PUT","DELETE"],
+        credentials:true,
+    })
+)
+
+app.use(express.json())
+
+app.use(router)
+app.listen(port,()=>{
+    console.log(`connection is live at port no, ${port}`)
+})
